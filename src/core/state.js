6a710a9d333e7b9c62a7a4f4f2e8346a795c8da9
Stefan Rimaila <stefan@rimaila.fi>
2019-11-11 18:01:06 +0200

    chore: add placeholder empty card

--- src/core/state.js
+++ src/core/state.js
@@ -8,7 +8,8 @@ const initialState = {
     {
       id: '4ec868f0-00bc-11ea-8a75-bf599f3916ed',
       title: 'A title',
-      body: 'A body and then some\n\n# heading\n\nand some more text',
+      body:
+        'A body and then some\n\n# heading\n\nand some more text\n\n- [ ] foo\n- [x] bar',
       position: [10, 10],
       size: [200, 250],
       flags: {
@@ -35,6 +36,17 @@ const initialState = {
       size: [150, 150],
       flags: {
         locked: true,
+        editing: false,
+      },
+    },
+    {
+      id: 'c330dd70-0491-11ea-b203-93415e273b1a',
+      title: 'empty boi',
+      body: '',
+      position: [420, 220],
+      size: [150, 150],
+      flags: {
+        locked: false,
         editing: false,
       },
     },
