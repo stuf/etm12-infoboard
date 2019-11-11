12aff0540cd60d4d5756cce92cc3a042aaeb95ce
Stefan Rimaila <stefan@rimaila.fi>
2019-11-11 18:01:29 +0200

    chore: housekeeping

--- src/scenes/Board/index.js
+++ src/scenes/Board/index.js
@@ -5,39 +5,16 @@ import { itemsIn } from 'common/meta';
 import { Card } from 'components';
 
 export default function Board(props) {
-  const actions = U.serializer(null);
-
-  console.log({ props });
-  const { state, match, history } = props;
+  const { state } = props;
 
   const cards = itemsIn(state);
 
-  const navigateToCardEff = item =>
-    U.doPush(actions, () =>
-      U.thru(
-        item,
-        U.view('id'),
-        U.consume(x => {
-          console.log('item =>', x);
-          history.push(`${match.url}?id=${x}`);
-        }),
-      ),
-    );
-
   return (
     <section className="board boardRoot">
-      <>{U.sink(actions)}</>
-
       <div className="boardCards">
         {U.thru(
           cards,
-          U.mapElems((it, i) => (
-            <Card
-              key={U.string`card-${i}`}
-              onSelectFn={navigateToCardEff}
-              item={it}
-            />
-          )),
+          U.mapElems((it, i) => <Card key={U.string`card-${i}`} item={it} />),
         )}
       </div>
     </section>
